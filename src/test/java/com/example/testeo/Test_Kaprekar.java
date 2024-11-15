18139cccd36bede1a61d80bc9bf09be9e72da23a
Strawberry Frappé <j.saez18@ufromail.cl>
2024-11-15 11:32:09 -0300

    Update Test_Kaprekar.java

--- src/test/java/com/example/testeo/Test_Kaprekar.java
+++ src/test/java/com/example/testeo/Test_Kaprekar.java
@@ -3,6 +3,9 @@ package com.example.testeo;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static com.example.dev.Main_Kaprekar.itKaprekar;
+import static com.example.dev.Main_Kaprekar.kaprekarOp;
+
 // revisar : https://plus.maths.org/content/mysterious-number-6174
 // basado en la operación de kaprekar se deben codificar 2 funciones:
 // 1) la función kaprekarOp realizará el ordenamiento y resta descrita en el algoritmo
@@ -14,6 +17,7 @@ public class Test_Kaprekar{
         @Test
         public void testKaprekarOperation1() {
             // Prueba con el número 3524 (debe devolver 3087)
+            // 5432 - 2345 = 3087
             Assertions.assertEquals(3087, kaprekarOp(3524));
         }
         @Test
